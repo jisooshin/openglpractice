5085525b555b6ce4494f211dccefd9e732775e45
jslab <jisoo.shin87@gmail.com>
2022-04-07 11:30:08 +0900

    개선된 버전의 model Loading dir 새로 파기

---
+++ model_loading/upgrad_version_of_model_loading/src/util.cpp
@@ -0,0 +1,141 @@
+#include "util.hpp"
+#ifndef STB_IMAGE_IMPLEMENTATION
+#define STB_IMAGE_IMPLEMENTATION
+#include "stb_image.h"
+#endif
+#ifndef STB_IMAGE_WRITE_IMPLEMENTATION
+#define STB_IMAGE_WRITE_IMPLEMENTATION
+#include "stb_image_write.h"
+#endif
+
+using namespace std;
+
+Shader::Shader(const char* vertexPath, const char* fragPath)
+{
+	string strVertexSource, strFragSource;
+	ifstream vShaderFile, fShaderFile;
+	vShaderFile.exceptions(ifstream::failbit | ifstream::badbit);
+	fShaderFile.exceptions(ifstream::failbit | ifstream::badbit);
+	try
+	{
+		vShaderFile.open(vertexPath);
+		fShaderFile.open(fragPath);
+		stringstream vShaderStream, fShaderStream;
+		vShaderStream << vShaderFile.rdbuf();
+		fShaderStream << fShaderFile.rdbuf();
+		vShaderFile.close(); fShaderFile.close();
+		strVertexSource = vShaderStream.str();
+		strFragSource   = fShaderStream.str();
+	}
+	catch(ifstream::failure e)
+	{
+		printf("ERROR::SHADER::FILE_CAN'T_READ\n");
+	}
+	const char* vSource = strVertexSource.c_str();
+	const char* fSource = strFragSource.c_str();
+
+	GLuint vertex, fragment;
+	int success { 0 };
+	char infoLog[512];
+
+	vertex = glCreateShader(GL_VERTEX_SHADER);
+	glShaderSource(vertex, 1, &vSource, NULL);
+	glCompileShader(vertex);
+	glGetShaderiv(vertex, GL_COMPILE_STATUS, &success);
+	if (!success)
+	{
+		glGetShaderInfoLog(vertex, 512, NULL, infoLog);
+		printf("ERROR::SHADER::VERTEX::COMPILATION_FAILED\n");
+		printf("%s\n", infoLog);
+	}
+
+	fragment = glCreateShader(GL_FRAGMENT_SHADER);
+	glShaderSource(fragment, 1, &fSource, NULL);
+	glCompileShader(fragment);
+	glGetShaderiv(fragment, GL_COMPILE_STATUS, &success);
+	if (!success)
+	{
+		glGetShaderInfoLog(fragment, 512, NULL, infoLog);
+		printf("ERROR::SHADER::FRAGMENT::COMPILATION_FAILED\n");
+		printf("%s\n", infoLog);
+	}
+
+	ID = glCreateProgram();
+	glAttachShader(ID, vertex);
+	glAttachShader(ID, fragment);
+	glLinkProgram(ID);
+
+	glGetProgramiv(ID, GL_LINK_STATUS, &success);
+	if (!success)
+	{
+		glGetProgramInfoLog(ID, 512, NULL, infoLog);
+		printf("ERROR::SHADER::PROGRAM::LINKING_FAILED\n%s\n", infoLog);
+	}
+	glDeleteShader(vertex); glDeleteShader(fragment);
+}
+
+
+void Shader::use()
+{
+	glUseProgram(ID);
+}
+
+void Shader::setBool(const string& name, bool value) const
+{
+	glUniform1i(glGetUniformLocation(ID, name.c_str()), (int)value);
+}
+
+void Shader::setInt(const string& name, int value) const
+{
+	glUniform1i(glGetUniformLocation(ID, name.c_str()), (int)value);
+}
+
+void Shader::setFloat(const string& name, float value) const
+{
+	glUniform1f(glGetUniformLocation(ID, name.c_str()), (float)value);
+}
+
+void Shader::setMat4(const string& name, glm::mat4 value) const
+{
+	glUniformMatrix4fv(glGetUniformLocation(ID, name.c_str()), 1, GL_FALSE, glm::value_ptr(value));
+}
+
+void Shader::setVec3(const string& name, glm::vec3 v) const
+{
+	glUniform3f(glGetUniformLocation(ID, name.c_str()), v[0], v[1], v[2]);
+}
+
+
+GLuint TextureFromFile(const char *path, const string &directory)
+{
+	stbi_set_flip_vertically_on_load(true);
+	string filename = string(path);
+	filename = directory + '/' + filename;
+	GLuint textureID;
+	glGenTextures(1, &textureID);
+	int width, height, nChannels;
+	unsigned char *data = stbi_load(filename.c_str(), &width, &height, &nChannels, 0);
+	if (data)
+	{
+		GLenum format;
+		if (nChannels == 1)
+			format = GL_RED;
+		else if (nChannels == 3)
+			format = GL_RGB;
+		else if (nChannels == 4)
+			format = GL_RGBA;
+ 
+		glBindTexture(GL_TEXTURE_2D, textureID);
+		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
+		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+		glTexImage2D(GL_TEXTURE_2D, 0, format, width, height, 0, format, GL_UNSIGNED_BYTE, data);
+		glGenerateMipmap(GL_TEXTURE_2D);
+	}
+	else
+	{
+		printf("Failed to read image file : %s", filename.c_str());
+	}
+	stbi_image_free(data);
+	return textureID;
+}
