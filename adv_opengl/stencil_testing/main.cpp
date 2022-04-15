f3b2c2b50cdaa69af629a8bf0ddb7ff1c71725cf
jslab <jisoo.shin87@gmail.com>
2022-04-15 17:56:54 +0900

    stencil testing 연습

--- adv_opengl/stencil_testing/main.cpp
+++ adv_opengl/stencil_testing/main.cpp
@@ -1,7 +1,7 @@
 #include "util.hpp"
 
-#define WINDOW_WIDTH 800
-#define WINDOW_HEIGHT 450
+#define WINDOW_WIDTH 1600	
+#define WINDOW_HEIGHT 800
 #define PROJ_NEAR    0.1f
 #define PROJ_FAR   100.0f
 
@@ -44,10 +44,32 @@ int main()
 	// global
 	glEnable(GL_DEPTH_TEST);
 	glDepthFunc(GL_LESS);
+	glEnable(GL_STENCIL_TEST);
+	// default mask = 0x00
+	glStencilFunc(GL_NOTEQUAL, 1, 0xFF);
+	glStencilOp(GL_KEEP, GL_KEEP, GL_REPLACE);
+	// glStencilOp(GL_KEEP, GL_KEEP, GL_KEEP);
+	// glStencilFunc(GL_NOTEQUAL, 1, 0xFF);
+	// glStencilOp(GL_KEEP, GL_KEEP, GL_REPLACE);
+
+
 	Shader _modelShader("../../shaders/models/ver.glsl", "../../shaders/models/frag.glsl");
 	Shader _lightShader("../../shaders/lights/ver.glsl", "../../shaders/lights/frag.glsl");
-	Model _backpack("../../data/sleeping_cat/sleeping_cat.obj");
+	Shader _outlineShader("../../shaders/models/outline/ver.glsl", "../../shaders/models/outline/frag.glsl");
+
+	Model _model1("../../data/tiber/Tibbers Hextech.obj");
+	Model _model2("../../data/sleeping_cat/sleeping_cat.obj");
+	Model _model3("../../data/backpack/backpack.obj");
+
 	Model _lightball("../../data/circle/circle.obj");
+
+	Model modelContaienr[3] = { _model1, _model2, _model3 };
+	float modelParamContainer[3][4] = 
+	{
+		{1.0f, 0.0f, 0.1f, 0.0f},
+		{0.3f, 2.0f, -0.75f, 2.0f},
+		{0.8f, -1.0f, 0.0f, 0.5f}
+	};
 
 
 	// - - - - - ↓ ↓ ↓ ↓ - - - - - // 
@@ -76,8 +98,6 @@ int main()
 	glBindVertexArray(0);
 	// - - - - - ↑ ↑ ↑ ↑ - - - - - // 
 
-
-
 	glfwSetInputMode(window, GLFW_CURSOR, GLFW_CURSOR_DISABLED);
 	glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
 	glfwSetCursorPosCallback(window, mouse_callback);
@@ -92,10 +112,11 @@ int main()
 
 		processInput(window);
 
-		glClearColor(0.1f, 0.1f, 0.1f, 1.0f);
-		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+		glClearColor(0.8f, 0.8f, 0.8f, 1.0f);
+		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT | GL_STENCIL_BUFFER_BIT);
 
 		glm::mat4 projection(1.0f);
+		glm::mat4 cameraView = camera.GetViewMatrix();
 		projection = glm::perspective(
 			glm::radians(camera.Zoom), (float)WINDOW_WIDTH / (float)WINDOW_HEIGHT, PROJ_NEAR, PROJ_FAR);
 
@@ -106,42 +127,66 @@ int main()
 		_light_model = glm::scale(_light_model, glm::vec3(0.2f));
 
 
-		glm::mat4 _model_view = camera.GetViewMatrix();
-		glm::mat4 _model_model(1.0f);
-		_model_model = glm::scale(_model_model, glm::vec3(1.0));
-		_model_model = glm::translate(_model_model, glm::vec3(0.0f, 0.1f, 0.0f));
-		// _model_model = glm::rotate(_model_model, glm::radians(90.0f), glm::vec3(0.0f, 0.0f, 0.0f));
+		glStencilFunc(GL_ALWAYS, 1, 0xFF);
+		glStencilMask(0xFF);
+		for (int i = 0; i < 3; i++)
+		{
+			glm::mat4 model(1.0f);
+			model = glm::scale(model, glm::vec3(modelParamContainer[i][0]));
+			model = glm::translate(model, glm::vec3(
+				modelParamContainer[i][1],
+				modelParamContainer[i][2],
+				modelParamContainer[i][3]));
 
-		_modelShader.use();
-		_modelShader.setMat4("view", _model_view);
-		_modelShader.setMat4("projection", projection);
-		_modelShader.setMat4("model", _model_model);
+			if (i == 1)
+			{
+				model = glm::rotate(model, glm::radians(90.0f), glm::vec3(0.0f, 1.0f, 0.0f));
+			}
+			_modelShader.use();
+			_modelShader.setMat4("view", cameraView);
+			_modelShader.setMat4("projection", projection);
+			_modelShader.setMat4("model", model);
+			_modelShader.setVec3 ("point.lv_Position", _light_position);
+			_modelShader.setFloat("point.lf_Constant", 1.0f);
+			_modelShader.setFloat("point.lf_LinearParam", 0.09f);
+			_modelShader.setFloat("point.lf_QuadParam", 0.032f);
+			_modelShader.setFloat("point.lf_Power", 2.0f);
+			_modelShader.setVec3 ("point.lv_CameraPosition", camera.Position);
+			modelContaienr[i].Draw(_modelShader);
 
-		_modelShader.setVec3 ("point.lv_Position", _light_position);
-		_modelShader.setFloat("point.lf_Constant", 1.0f);
-		_modelShader.setFloat("point.lf_LinearParam", 0.09f);
-		_modelShader.setFloat("point.lf_QuadParam", 0.032f);
-		_modelShader.setFloat("point.lf_Power", 2.0f);
-		_modelShader.setVec3 ("point.lv_CameraPosition", camera.Position);
-
-		_backpack.Draw(_modelShader);
+			// glStencilFunc(GL_NOTEQUAL, 1, 0xFF);
+			// glStencilMask(0x00);
+			// model = glm::scale(model, glm::vec3(1.02f));
+			// _outlineShader.use();
+			// _outlineShader.setMat4("view", cameraView);
+			// _outlineShader.setMat4("projection", projection);
+			// _outlineShader.setMat4("model", model);
+			// _outlineShader.setVec3 ("point.lv_Position", _light_position);
+			// _outlineShader.setFloat("point.lf_Constant", 1.0f);
+			// _outlineShader.setFloat("point.lf_LinearParam", 0.09f);
+			// _outlineShader.setFloat("point.lf_QuadParam", 0.032f);
+			// _outlineShader.setFloat("point.lf_Power", 2.0f);
+			// _outlineShader.setVec3 ("point.lv_CameraPosition", camera.Position);
+			// modelContaienr[i].Draw(_outlineShader);
+		}
 
 
 		_lightShader.use();
-		_lightShader.setMat4("view", _model_view);
+		_lightShader.setMat4("view", cameraView);
 		_lightShader.setMat4("projection", projection);
 		_lightShader.setMat4("model", _light_model);
 		_lightball.Draw(_lightShader);
 
 
 		// - - - - - ↓ ↓ ↓ ↓ - - - - - // 
+		glStencilMask(0x00);
 		glBindVertexArray(planeVAO);
 		_planeShader.use();
 		glm::mat4 _plane_model = glm::mat4(1.0f);
 		_plane_model = glm::translate(_plane_model, glm::vec3(0.0f, 0.0f, 0.0f));
 		_plane_model = glm::scale(_plane_model, glm::vec3(2.0f, 2.0f, 2.0f));
 		_planeShader.setMat4("model", _plane_model);
-		_planeShader.setMat4("view", _model_view);
+		_planeShader.setMat4("view", cameraView);
 		_planeShader.setMat4("projection", projection);
 		_planeShader.setInt("floorTexture", 0);
 		glActiveTexture(GL_TEXTURE0);
